0def13728ce8c4c00ebd0fc8ab32aaa5954644b3
Lény <leny-07@hotmail.fr>
2021-01-09 18:17:45 +0100

    modification SIR

--- ProjetAPO/src/projetapo/Sir.java
+++ ProjetAPO/src/projetapo/Sir.java
@@ -48,20 +48,31 @@ public class Sir {
      * Affiche chaque jour le nouveau nombre de personnes pour chaque catégories
      */
     public void simulationSIR() {
-        double s = 0;
-        double i = 0;
+        double S = p.getS(), I = p.getI(), R = p.getR();
+        double new_infecte = 0;
+        double N = p.getS() + p.getI() + p.getR();
         
-        System.out.println("Jour 0 : S = " + p.getS() + " I = " + p.getI()
-                + " R = " + p.getR());
+        System.out.println("Jour 0 : S = " + S + " I = " + I + " R = " + R);
 
         for (int j = 1; j <= temps; j++) {
-            s = p.getS();
-            i = p.getI();
-            p.setS(s + (-beta * s * i));
-            p.setI(i + (beta * s * i) - (gamma * i));
-            p.setR(p.getR() + gamma * i);
-            System.out.println("Jour " + j + " : S = " + p.getS() + " I = " + p.getI()
-                    + " R = " + p.getR());
+            new_infecte = beta * S * I;
+            
+            // Gère le cas d'une épidémie 'violente' pour ne pas aggrandir la population totale
+            if (new_infecte > S){
+                p.setS(0);
+                p.setI(I + S - (gamma * I));
+            }
+            else {
+                p.setS(S - new_infecte);
+                p.setI(I + new_infecte - (gamma * I));
+            }
+            p.setR(R + gamma * I);
+            
+            S = p.getS();
+            I = p.getI();
+            R = p.getR();
+            
+            System.out.println("Jour " + j + " : S = " + S + " I = " + I + " R = " + R);
         }
     }
 
