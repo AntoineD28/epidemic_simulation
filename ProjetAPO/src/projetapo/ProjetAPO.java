03ac42ad214c087802f2a75c8c25fa7cde8d7290
Lény <leny-07@hotmail.fr>
2021-01-09 18:18:54 +0100

    modification main

--- ProjetAPO/src/projetapo/ProjetAPO.java
+++ ProjetAPO/src/projetapo/ProjetAPO.java
@@ -16,8 +16,16 @@ public class ProjetAPO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Sir s = new Sir(0.00218, 0.44, 762, 1, 0, 30);
+        Sir s = new Sir(0.8, 0.44, 762, 1, 0, 30);
         s.simulationSIR();
+        System.out.println();
+        
+        Seir s2 = new Seir(0.8, 0.2, 0.5, 600, 100, 400, 0, 30);
+        s2.simulationSEIR();
+        System.out.println();
+        
+        SeirN s3 = new SeirN(0.8, 0.2, 0.5, 0.05, 0.06, 600, 100, 400, 0, 30);
+        s3.simulationSEIRN();
     }
     
 }
