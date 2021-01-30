d89fe7caa772c8b23e9d2fdf75c692c118513028
Lény <leny-07@hotmail.fr>
2021-01-30 23:53:38 +0100

    Modification Seir

--- ProjetAPO/src/projetapo/Seir.java
+++ ProjetAPO/src/projetapo/Seir.java
@@ -14,8 +14,10 @@ import java.util.ArrayList;
  */
 public class Seir extends Modele{
     
-    /* parametre alpha */
-    private double alpha;
+    /**
+     * parametre alpha
+     */
+    private double alpha; 
 
     /**
      * Constructeur par défaut
@@ -25,7 +27,7 @@ public class Seir extends Modele{
     }
 
     /**
-     * Constructeur avec pramètres (sans la spacialisation et les politiques publiques)
+     * Constructeur avec pramètres (sans la spatialisation et les politiques publiques)
      * @param b paramètre de transmission
      * @param a paramètre d'incubation
      * @param g paramètre de guérison
@@ -36,11 +38,11 @@ public class Seir extends Modele{
      * @param t temps (en jours)
      */
     public Seir(double b, double a, double g, double s, double e, double i, double r, int t) {
-        this(b,a,g,s,e,i,r,t,1,1,false,false,false,false,0);
+        this(b,a,g,s,e,i,r,t,1,1,false,false,false,false,0,0);
     }
     
     /**
-     * Constructeur avec pramètres (avec la spacialisation mais sans politique publique)
+     * Constructeur avec pramètres (avec la spatialisation mais sans politique publique)
      * @param b paramètre de transmission
      * @param a paramètre d'incubation
      * @param g paramètre de guérison
@@ -53,11 +55,11 @@ public class Seir extends Modele{
      * @param M largeur du monde
      */
     public Seir(double b, double a, double g, double s, double e, double i, double r, int t, int N, int M) {
-        this(b,a,g,s,e,i,r,t,N,M,false,false,false,false,0);
+        this(b,a,g,s,e,i,r,t,N,M,false,false,false,false,0,0);
     }
     
     /**
-     * Constructeur avec pramètres (cas sans vaccination)
+     * Constructeur avec pramètres (cas sans vaccination et quarantaine)
      * @param b paramètre de transmission
      * @param a paramètre d'incubation
      * @param g paramètre de guérison
@@ -75,7 +77,7 @@ public class Seir extends Modele{
      */
     public Seir(double b, double a, double g, double s, double e, double i, double r, int t, int N, int M, 
             boolean c, boolean ma, boolean q, boolean v) {
-        this(b,a,g,s,e,i,r,t,N,M,c,ma,q,v,0);
+        this(b,a,g,s,e,i,r,t,N,M,c,ma,q,v,0,0);
     }
     
     /**
@@ -94,36 +96,22 @@ public class Seir extends Modele{
      * @param ma port du masque
      * @param q quarantaine
      * @param v vaccination 
+     * @param tq Durée de la mise en quarantaine
      * @param pv Probabilité qu'une personne saine se fasse vacciner
      */
     public Seir(double b, double a, double g, double s, double e, double i, double r, int t, int N, int M, boolean c, 
-            boolean ma, boolean q, boolean v, double pv) {
-        super(b,g,s,e,i,r,t,N,M,c,ma,q,v,pv); // Appel du constructeur de Modele
+            boolean ma, boolean q, boolean v, int tq, double pv) {
+        super(b,g,s,e,i,r,t,N,M,c,ma,q,v,tq,pv); // Appel du constructeur de Modele
         alpha = a;
     }
     
     
-    
+
     /**
-     * Méthode qui gère les nouveaux exposés sans l'utilisation de la spacialisation.
-     * @param exposées Nombre de nouvelles personnes exposées
-     */
-    public void exposition(int exposées){
-        Population p = m.getPopulation();
-        ArrayList<Personne> Exposé = p.getE();
-        ArrayList<Personne> Sain = p.getS();
-        
-        for (int i=0; i<exposées; i++){
-            Exposé.add(Sain.get(0));
-            Sain.remove(0);
-        }
-    }
-    
-    /**
-     * Méthode qui gère les nouvelles personnes exposées avec utilisation de la spacialisation.
+     * Méthode qui gère les nouvelles personnes exposées.
      * Test si une personnes saine et une infectée sont sur la même case, si c'est le cas, la personne saine à une probabilité beta de devenir exposée
      */
-    public void expositionSpacialisation(){
+    public void exposition(){
         Population p = m.getPopulation();
         ArrayList<Personne> Sain = p.getS();
         ArrayList<Personne> Exposé = p.getE();
@@ -136,17 +124,19 @@ public class Seir extends Modele{
         //On compare la position de tous les sains avec tous les infectées
         while (i<Sain.size() && j<nb_inf){
                 supr = false;
-                boolean b = Sain.get(i).comparePosition(Inf.get(j));    // Compare la position d'une personne saine et d'1 infectée
-                if(b){  // Si les 2 personnes sont sur la même case
-                    if (Sain.get(i).getMasque()) // Si la personne porte le masque
-                        bet = beta/10;        // Le parametre beta est divisé par 10
-                    else 
-                        bet = beta;         // Sinon, on garde le parametre beta de base
-                    r = Math.random();      // r prend une valeur entre 0 et 1
-                    if(r<=bet){             // Si r<bet, la personne saine devient exposée, c.a.d qu'on a une proba "bet" de devenir exposée 
-                        Exposé.add(Sain.get(i));
-                        Sain.remove(Sain.get(i));
-                        supr = true;
+                if (!Inf.get(j).getQuarantaine()){ //Si la personne infecté n'est pas en quarantaine, elle peut infecter 1 personne, sinon non (donc le test n'est pas utile)
+                    boolean b = Sain.get(i).comparePosition(Inf.get(j));    // Compare la position d'une personne saine et d'1 infectée
+                    if(b){  // Si les 2 personnes sont sur la même case
+                        if (Sain.get(i).getMasque()) // Si la personne porte le masque
+                            bet = beta/10;        // Le parametre beta est divisé par 10
+                        else 
+                            bet = beta;         // Sinon, on garde le parametre beta de base
+                        r = Math.random();      // r prend une valeur entre 0 et 1
+                        if(r<=bet){             // Si r<bet, la personne saine devient exposée, c.a.d qu'on a une proba "bet" de devenir exposée 
+                            Exposé.add(Sain.get(i));
+                            Sain.remove(Sain.get(i));
+                            supr = true;
+                        }
                     }
                 }
                 j++;    //Itération sur le parcours des infectées
@@ -163,7 +153,6 @@ public class Seir extends Modele{
      * Méthode qui gère les nouvelles personnes infectées.
      * @param inf Nombre de nouvelles personnes infectées
      */
-    @Override
     public void infection(int inf){
         Population p = m.getPopulation();
         ArrayList<Personne> Inf = p.getI();
@@ -179,64 +168,26 @@ public class Seir extends Modele{
     
     
     /**
-     * Procédure qui réalise la simulation du modèle SEIR sans spacialisation.
+     * Procédure qui réalise la simulation du modèle SEIR.
      * Affiche chaque jour le nouveau nombre de personnes pour chaque catégories
      */
     @Override
     public void simulation() {
         Population p = m.getPopulation();
         int S = p.getS().size(), E = p.getE().size(), I = p.getI().size(), R = p.getR().size();
-        double new_I = 0, new_R = 0, new_E = 0;
-      
-        System.out.println("Jour 0 : S = " + S + " E = " + E + " I = " + I + " R = " + R); // Affichage du jour 0
-
-        for (int j = 1; j <= temps; j++) { // Boucle le nombre de jour choisi pour la simulation
-            // Calcul du nombre d'ajout dans chaque population
-            new_E += beta * S * I - (int)new_E;
-            new_I += alpha * E - (int)new_I;
-            new_R += gamma * I - (int)new_R;
-            
-            // Modifie la population exposée
-            if (new_E > S){ // Gère le cas d'une épidémie 'violente' pour ne pas aggrandir la population totale
-                exposition(S);
-            }
-            else {
-                exposition((int)new_E);
-            }
-            
-            // Modifie la population
-            infection((int)new_I);
-            guerison((int)new_R);
-            
-            // Nombre de personne dans chaque catégories
-            S = p.getS().size();
-            E = p.getE().size();
-            I = p.getI().size();
-            R = p.getR().size();
-            
-            // Affichage
-            System.out.println("Jour " + j +  " : S = " + S + " E = " + E + " I = " + I + " R = " + R);
-        }
-    }
-    
-    /**
-     * Procédure qui réalise la simulation du modèle SEIR avec spacialisation.
-     * Affiche chaque jour le nouveau nombre de personnes pour chaque catégories
-     */
-    @Override
-    public void simulationSpacialisation() {
-        Population p = m.getPopulation();
-        int S = p.getS().size(), E = p.getE().size(), I = p.getI().size(), R = p.getR().size();
         double new_R = 0, new_I = 0, nbPersVaccin;
+        double N = S+E+I+R;
+
         
-        System.out.println("Jour 0 : S = " + S + " E = " + E + " I = " + I + " R = " + R); // Affichage du jour 0
+        System.out.println("Jour 0 : Population = " + N + " S = " + S + " E = " + E + " I = " + I
+                + " R = " + R); // Affichage du jour 0
 
         for (int j = 1; j <= temps; j++) { // Boucle le nombre de jour choisi pour la simulation
             // Calcul du nombre d'ajout dans chaque population
             new_R += gamma * I - (int)new_R;
             new_I += alpha * E - (int)new_I;
             
-            // Afficher la population et le monde (utilisé pour vérifier que la spacialisation marche bien)
+            // Afficher la population et le monde (utilisé pour vérifier que la spatialisation marche bien)
             /*p.afficherPopulation();
             m.afficherMonde();*/
             
@@ -244,10 +195,15 @@ public class Seir extends Modele{
             p.prochainDeplacement(confinement);
             p.deplacer();
             
+            // Gère les personnes infectées qui sont en quarantaine
+            if (quarantaine){
+                quarantaine();
+            }
+            
             // Modifie la population
-            expositionSpacialisation();
+            exposition();
             infection((int)new_I);
-            guerison((int)new_R);
+            retire((int)new_R);
                    
             // Gere les vaccinations 
             if (vaccination){
@@ -260,9 +216,11 @@ public class Seir extends Modele{
             E = p.getE().size();
             I = p.getI().size();
             R = p.getR().size();
+            N = S+E+I+R;
             
             // Affichage
-            System.out.println("Jour " + j +  " : S = " + S + " E = " + E + " I = " + I + " R = " + R);
+            System.out.println("Jour " + j +  " Population = " + N + " S = " + S + " E = " + E + " I = " + I
+                + " R = " + R);
         }
     }
     
